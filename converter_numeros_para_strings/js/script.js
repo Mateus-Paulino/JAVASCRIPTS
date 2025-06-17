4c11c83afa72acceb17feb85fcbb5f9dfc35889b
Mateus Paulino <mateuspaulino.py@gmail.com>
2025-06-17 20:26:07 -0300

    2 Métodos para converter Numero para String
    Conversões_JS

---
+++ converter_numeros_para_strings/js/script.js
@@ -0,0 +1,11 @@
+// Number to String
+
+// Method 1
+//const num = 123;  123
+//const str = String(num);
+//console.log(str);
+
+// Method 2
+const num = 123;
+const str = num.toString();
+console.log(str);
