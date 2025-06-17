ec38d1936be3497815b99cf50d445ca9ca667813
Mateus Paulino <mateuspaulino.py@gmail.com>
2025-06-17 11:17:11 -0300

    menus_dependentes_api_IBGEBrasil
    EstadosIBGE

---
+++ menus_dependentes_em_formulario/js/script.js
@@ -0,0 +1,72 @@
+const ufSelect = document.getElementById('uf');
+
+const cidadeSelect = document.getElementById('cidade');
+
+const formInput = document.getElementById('codigo-cidade');
+
+fetch('https://servicosdados.ibge.gov.br/api/v1/localidade/estado?')
+.then(response => response.json())
+.then(estados =>{
+    ufSelect.innerHTML = '<option value="">Selecione um Estado</option>'
+
+    estados.forEach(estados => {
+        const option = document.createElement('option');
+
+            option.value = estado.sigla;
+
+            option.setAttribute('data-id', estado.id);
+
+            option.textContent = estado.nome;
+
+            ufSelect.appendChild(option);
+        });
+});
+
+ufSelect.addEventListener('change', function(){
+    const selectedUF = this.value;
+
+    const estadoId = this.selectedOptions[0].getAttribute('data-id');
+
+    cidadeSelect.innerHTML = '<option value="">Carregando cidades...</option>';
+
+    cidadeSelect.disabled = true;
+
+    codigoInput.value = '';
+    codigoInput.disabled = true;
+
+    if (!selectedUF) return;
+
+    fetch(`https://servicodados.ibge.gov.br/api/v1/localidade/estados/${estadoId}/municipios`)
+        .then(response => response.json())
+        .then(cidades => {
+            cidadeSelect.innerHTML = '<option value="">Selecione a cidade...';
+
+            cidades.forEach(cidades => {
+                const option = document.createElement('option');
+
+                option.value = cidade.nome;
+
+                option.setAttribute('data-id', cidade.id);
+
+                option.textContent = cidade.nome;
+
+                cidadeSelect.appendChild('option');
+            });
+
+            cidadeSelect.disabled = false;
+        });
+});       
+
+cidadeSelect.addEventListener('change', function() {
+    const selectedOption = this.selectedOption[0];
+
+    const codigoIBGE = selectedOption.getAttribute('data-id');
+
+    if (codigoIBGE) {
+        codigoInput.value = codigoIBGE;
+        codigoInput.disabled = false;
+    } else {
+        codigoInput.value = '';
+        codigoInput.disable = true;
+    }
+});
