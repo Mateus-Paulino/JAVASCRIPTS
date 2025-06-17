c4f7c9a30feee73bdf47e58cacdd68a124682e16
Mateus Paulino <mateuspaulino.py@gmail.com>
2025-06-17 17:54:57 -0300

    Operadores_ternário_?_:
    S2

---
+++ operador_ternario_em_javascript/js/script.js
@@ -0,0 +1,16 @@
+//condition ? expressionIfTrue : expressionIfFalse
+
+//? means if true
+//: means else
+
+//const age = 10;
+
+//const canVote = (age >= 18) ? "Yes, you can vote" : "No, you cant";
+
+//console.log(canVote);
+
+const random = Math.random(); // the random value is between 0 and 1
+console.log(random);
+const result = (random>0.5) ? "Big Number" : "Small Number";
+
+console.log(result);
