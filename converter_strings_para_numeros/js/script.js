d8680d9c0c539975fb0e858deb5ef8f56709240d
Mateus Paulino <mateuspaulino.py@gmail.com>
2025-06-17 20:13:34 -0300

    4 Métodos para converter String para Numero
    Conversões_JS

---
+++ converter_strings_para_numeros/js/script.js
@@ -0,0 +1,21 @@
+// String to Number like this : "45" "76764" "412426213.996"
+
+// Method 1
+//const str = "45"; 45
+//const num = Number(str);
+//console.log(num);
+
+// Method 2 parseInt()
+//const str = "34.67";   34
+//const num = parseInt(str);
+//console.log(num);
+
+// Method 3 parseFloat()
+//const str = "34.67";   34.67
+//const num = parseFloat(str);
+//console.log(num);
+
+// Method 4 
+///const str = "99";
+//const num = +str;
+//console.log(num);
