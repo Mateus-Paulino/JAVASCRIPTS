97768e4b0fb05af667c436b5e673920777ee49e4
Mateus Paulino <mateuspaulino.py@gmail.com>
2025-06-18 09:33:57 -0300

    Verificação de valores
    check indefinidos e nulos

---
+++ indefinidos_ou_nulos/js/script.js
@@ -0,0 +1,20 @@
+// undefined
+// A variable that is declared but not assigned(recebeu) any value yet.
+//let a;
+//console.log(a);
+
+// null 
+// A value that intentionally(intencionalmente) represents "no value" or "null".
+let a = null;
+console.log(a);
+
+if (a === null) {
+    console.log('null is printed');
+}
+
+// Real- world Use Cases
+//Checking if API response is empty.
+
+//Checking if a user input is missing.
+
+//handling optional properties in objects.
