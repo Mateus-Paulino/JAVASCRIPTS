0f0d2b35019c884a53b089f1602416631d9b6060
Mateus Paulino <mateuspaulino.py@gmail.com>
2025-06-18 09:12:49 -0300

    Mudar a primeira letra da String
    Capitalize e enfim

---
+++ primeira_letra_string_em_maiuscula/js/script.js
@@ -0,0 +1,18 @@
+// Capitalize the first letter
+
+// chatAt(0) - to get the first letter
+
+// toUpperCase() - to make it UpperCase
+
+// slice(1) - to get the rest of the string
+
+// str.charAt(0).toUpperCase() + str.slice(1)
+
+// patrick
+// Patrick
+
+
+let word = "jeremias";
+let Capitalize = word.charAt(0).toUpperCase() + word.slice(1);
+
+console.log(Capitalize);
