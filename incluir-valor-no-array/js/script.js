f33136979b3c306a474b3699805d56c2359d4343
Mateus Paulino <mateuspaulino.py@gmail.com>
2025-07-11 16:55:20 -0300

    Métodos-includes()indeOf()
    usados para verificação de ARRAYS 🔑

---
+++ incluir-valor-no-array/js/script.js
@@ -0,0 +1,19 @@
+// ["apple", "banana", "mango"]
+
+
+// includes() - better method
+// let frutas = ["apple", "banana", "mango"];
+
+// console.log(frutas.includes("banana"));
+// True
+// console.log(frutas.includes("grapes"));
+// False
+// const truth = frutas.includes("banana");
+// console.log(truth);
+
+
+// indexOf() - should learn
+let frutas = ["apple", "banana", "mango"];
+
+console.log(frutas.indexOf("mango") !== -1); // True
+console.log(frutas.indexOf("grapes") !== -1); // False
